722f6b372ca38030a7458ae94f96fdf330104aef
francesco <francesco.protopapa22@gmail.com>
2023-12-07 16:33:35 +0100

    Refactor

--- lib/Day_07.ml
+++ lib/Day_07.ml
@@ -1,22 +1,32 @@
 let invalid_input = Invalid_argument "Invalid input"
 
-let parse_input str =
+let parse_input str comp =
   String.split_on_char '\n' str
   |> List.map (fun s ->
          match String.split_on_char ' ' s with
          | [ x; y ] -> (x, y)
          | _ -> raise invalid_input)
+  |> List.sort (fun (x, _) (y, _) -> comp x y)
+  |> List.map (fun (_, it) -> int_of_string it)
 
-let rec get_occurences chars prev amount =
+let rec get_occurrences chars prev amount =
   match chars with
   | [] -> amount :: []
   | c :: cs ->
-      if c = prev then get_occurences cs c (amount + 1)
-      else amount :: get_occurences cs c 1
+      if c = prev then get_occurrences cs c (amount + 1)
+      else amount :: get_occurrences cs c 1
 
-let get_occurences_2 chars prev amount wildcards =
-  let occ = get_occurences chars prev amount |> List.sort (fun x y -> y - x) in
+let get_occurrences_2 chars prev amount wildcards =
+  let occ = get_occurrences chars prev amount |> List.sort (fun x y -> y - x) in
   match occ with x :: xs -> (x + wildcards) :: xs | _ -> raise invalid_input
+
+let compute_score occurrences =
+  match (List.length occurrences, occurrences) with
+  | 1, _ -> 7
+  | 2, c :: _ -> if c = 1 || c = 4 then 6 else 5
+  | 3, [ c1; c2; c3 ] -> if c1 = 3 || c2 == 3 || c3 == 3 then 4 else 3
+  | 4, _ -> 2
+  | _, _ -> 1
 
 let get_score str =
   let chars =
@@ -26,13 +36,7 @@ let get_score str =
   let c, cs =
     match chars with x :: xs -> (x, xs) | [] -> raise invalid_input
   in
-  let occurences = get_occurences cs c 1 in
-  match (List.length occurences, occurences) with
-  | 1, _ -> 7
-  | 2, c :: _ -> if c = 1 || c = 4 then 6 else 5
-  | 3, c1 :: c2 :: c3 :: _ -> if c1 = 3 || c2 == 3 || c3 == 3 then 4 else 3
-  | 4, _ -> 2
-  | _, _ -> 1
+  compute_score (get_occurrences cs c 1)
 
 let get_score_2 str =
   if str = "JJJJJ" then 7
@@ -45,66 +49,35 @@ let get_score_2 str =
     let c, cs =
       match chars with x :: xs -> (x, xs) | [] -> raise invalid_input
     in
-    let occurences =
-      get_occurences_2 cs c 1 (String.length str - List.length chars)
+    let occurrences =
+      get_occurrences_2 cs c 1 (String.length str - List.length chars)
     in
-    match (List.length occurences, occurences) with
-    | 1, _ -> 7
-    | 2, c :: _ -> if c = 1 || c = 4 then 6 else 5
-    | 3, c1 :: c2 :: c3 :: _ -> if c1 = 3 || c2 == 3 || c3 == 3 then 4 else 3
-    | 4, _ -> 2
-    | _, _ -> 1
+    compute_score occurrences
 
-let map_seed card =
+let map_seed wildcard_val card =
   match card with
   | 'A' -> 'e'
   | 'K' -> 'd'
   | 'Q' -> 'c'
-  | 'J' -> 'b'
+  | 'J' -> wildcard_val
   | 'T' -> 'a'
   | _ -> card
 
-let map_seed_2 card =
-  match card with
-  | 'A' -> 'e'
-  | 'K' -> 'd'
-  | 'Q' -> 'c'
-  | 'J' -> '0'
-  | 'T' -> 'a'
-  | _ -> card
-
-let compare card1 card2 =
+let compare wildcard_val card1 card2 =
   if get_score card1 <> get_score card2 then get_score card1 - get_score card2
-  else String.compare (String.map map_seed card1) (String.map map_seed card2)
-
-let compare_2 card1 card2 =
-  if get_score_2 card1 <> get_score_2 card2 then
-    get_score_2 card1 - get_score_2 card2
   else
-    String.compare (String.map map_seed_2 card1) (String.map map_seed_2 card2)
+    String.compare
+      (String.map (map_seed wildcard_val) card1)
+      (String.map (map_seed wildcard_val) card2)
 
-let part_1 str =
-  let sorted_input =
-    parse_input str
-    |> List.sort (fun (x, _) (y, _) -> compare x y)
-    |> List.map (fun (_, it) -> int_of_string it)
+let get_total_score sorted_input =
+  let _, res =
+    List.fold_left
+      (fun (index, acc) bid -> (index + 1, acc + (bid * index)))
+      (1, 0) sorted_input
   in
-  List.fold_left2
-    (fun acc l1 l2 -> acc + (l1 * l2))
-    0
-    (List.init (List.length sorted_input) (fun it -> it + 1))
-    sorted_input
+  res
 
-let part_2 str =
-  let sorted_input =
-    parse_input str
-    |> List.sort (fun (x, _) (y, _) -> compare_2 x y)
-    |> List.map (fun (_, it) -> int_of_string it)
-  in
-  List.fold_left2
-    (fun acc l1 l2 -> acc + (l1 * l2))
-    0
-    (List.init (List.length sorted_input) (fun it -> it + 1))
-    sorted_input
-
+let part_1 str = parse_input str (compare 'b') |> get_total_score
+let part_2 str = parse_input str (compare '0') |> get_total_score
 let main input = (part_1 input, part_2 input)
